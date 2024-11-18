455a5c5ff948aa9816d992652fba6dddce711d09
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-11-18 16:04:48 -0400

    added tooltips, exp functionality, and navbar bg on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { experience } from "./objects.js"
+
 // ============ burger menu animations ============
 const burgerMenu = document.querySelector('.burgerMenu')
 const mobileNav = document.querySelector('.navLinks')
@@ -12,3 +14,58 @@ mobileNavLinks.forEach((link) => {
         mobileNav.classList.remove('openMobileNav')
     })
 })
+
+
+// ============ navbar background on scroll =============
+window.onscroll = () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        document.querySelector('.siteHeader').style.background = 'white'
+        document.querySelector('.siteHeader').style.borderBottom = '1px solid lightgrey'
+    } else {
+        document.querySelector('.siteHeader').style.background = 'none'
+        document.querySelector('.siteHeader').style.borderBottom = 'none'
+    }
+}
+
+
+
+
+// ========= render experiences ===========
+const defaultExpCategory = 'Work'
+filterExperience(defaultExpCategory)
+
+function filterExperience(category){
+    let filteredExperiences = experience.filter((exp, index) => {
+        return exp.type === category
+    })
+
+    renderExperiences(filteredExperiences)
+}
+
+        //function to render all the experiences
+function renderExperiences(expToRender){
+    let experienceContainer = document.querySelector('.experienceContainer')
+    console.log(expToRender)
+    let experienceHTML = ''
+
+    expToRender.forEach((exp) => {
+
+        experienceHTML += `
+            <p>${exp.title}</p>
+            <p style = 'margin-bottom: 1rem;'>${exp.type}</p>
+        `
+    })
+
+    experienceContainer.innerHTML = experienceHTML
+}
+
+
+        //code to add filter ability to buttons
+const categoryItems = document.querySelectorAll('.experienceCategories button')
+
+categoryItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        filterExperience(item.innerHTML)
+    })
+})
+
---
+++ scripts/objects.js
@@ -0,0 +1,62 @@
+export const experience = [
+    {
+        duration: 'Oct 2024 - Present',
+        type: 'Certificate',
+        title: 'UX Design - Google (Coursera)',
+        description: [
+            'this is a pointer',
+            'this is a pointer description 2'
+        ]
+    }, 
+    {
+        duration: 'June 2024 - Aug 2024',
+        type: 'Work',
+        title: 'Information Technology Intern - Tucker Energy Services Ltd.',
+        description: [
+            'this is a pointer',
+            'this is a pointer description 2'
+        ]
+    },
+    {
+        duration: 'Sept 2021 - June 2024',
+        type: 'Education',
+        title: 'BSc. Computer Science - The University of the West Indies',
+        description: [
+            'Graduated with a GPA of 3.31'
+        ]
+    },
+    {
+        duration: 'Feb 2021 - April 2021',
+        type: 'Certificate',
+        title: 'Web Design - CTS College of Business and Computer Science',
+        description: [
+            'Completed course with a grade of A+'
+        ]
+    },
+    {
+        duration: 'Sept 2018 - June 2020',
+        type: 'Education',
+        title: `CAPE - Queen's Royal College`,
+        description: [
+            'Applied Mathematics - Unit 1: III & Unit 2: II',
+            'Pure Mathematics - Unit 1: IV & Unit 2: I',
+            'Physics - Unit 1: III & Unit 2: I',
+            'Caribbean Studies - II',
+            'Communication Studies - II'
+        ]
+    },
+    {
+        duration: 'Sept 2016 - June 2018',
+        type: 'Education',
+        title: `CSEC - St. Augustine Community College`,
+        description: [
+            'Additional Mathematics - II',
+            'Chemistry - II',
+            'English A - II',
+            'Information Technology - I',
+            'Mathematics - I',
+            'Physics - I',
+            'Principle of Accounts - I'
+        ]
+    }
+]

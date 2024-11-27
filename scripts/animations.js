cee37faa84062775a4a7ff886a482832f4eb5ea1
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-11-26 20:52:43 -0400

    added animations on scroll

--- scripts/animations.js
+++ scripts/animations.js
@@ -60,41 +60,111 @@ document.addEventListener('DOMContentLoaded', (event) => {
   .from('.mainContent h1', {
     opacity: 0,
     duration: 0.2
-  })
+  }, '-=0.5')
   .from('.tag', {
     duration: 0.9, 
     stagger: 0.1,
     ease: Expo.easeInOut,
     opacity: 0
-  })
+  }, '-=0.6')
   .from('.aboutMeText',{
     y: '10px',
     opacity: 0,
     duration: 0.5
-  })
+  }, '-=0.5')
 
   .from('.socialLinks a', {
     y: '10px',
     opacity: 0,
     stagger: 0.1
-  })
+  }, '-=0.5')
   .from('.myStack h3', {
     opacity: 0
-  })
+  }, '-=0.6')
   .from('.langCard', {
     y: '10px',
     opacity: 0,
     stagger: 0.1
-  })
+  }, '-=0.7')
   .from('.blob', {
     opacity: 0,
     stagger: 0.1
-  })
+  }, '-=0.4')
 
   .from('.location, .themeToggle', {
     y: '10px',
     opacity: 0,
     stagger: 0.1
+  }, '-=0.5')
+
+
+  // scroll trigger
+
+  var scrollTL = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#experienceSection',
+      start: 'top center'
+    }
   })
+  scrollTL.from('#experienceSection .sectionTitle', {
+    y: '5px',
+    opacity: 0,
+    duration: 0.5
+  })
+  scrollTL.from('.experienceCategories button', {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.5,
+    stagger: 0.1
+  }, '-=0.4')
+  scrollTL.from('.experienceContainer', {
+    opacity: 0,
+    duration: 0.5
+  }, '-=0.5')
+
+
+  var scrollTL_1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#portfolioSection',
+      start: 'top center'
+    }
+  })
+  scrollTL_1.from('#portfolioSection .sectionTitle', {
+    y: '5px',
+    opacity: 0,
+    duration: 0.5
+  })
+  scrollTL_1.from('.projectsCategories, .categoryList li', {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.5,
+    stagger: 0.1
+  }, '-=0.4')
+
+
+  scrollTL_1.from('.projectsContainer', {
+    opacity: 0,
+    duration: 0.5
+  }, '-=0.5')
+
+
+  var scrollTL_2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#contactSection',
+      start: 'top center'
+    }
+  })
+  scrollTL_2.from('#contactSection .sectionTitle', {
+    y: '5px',
+    opacity: 0,
+    duration: 0.5
+  })
+  scrollTL_2.from('.formInput label, .formInput input, .formInput textarea, #submitBtn', {
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.5,
+    stagger: 0.1
+  }, '-=0.4')
 
 })
+
